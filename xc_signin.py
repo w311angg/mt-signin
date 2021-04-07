08ece11d0387e3b7d19aeeb782cd1201f1873acb
RZ <30381083+w311ang@users.noreply.github.com>
2021-04-08 07:23:47 +0800

    Create xc_signin.py

---
+++ xc_signin.py
@@ -0,0 +1,12 @@
+import requests
+import os
+
+s=requests.Session()
+username=os.getenv('xc_username')
+password=os.getenv('xc_password')
+
+s.get('https://cdn.79tian.com/api/wxapi/view/AppSwitch.php?inc=ajax_login&act=BannerHome')
+token=s.get('https://cdn.79tian.com/api/wxapi/view/AppSwitch.php?inc=ajax_login&act=login&username=%s&password=%s'%(username,password)).json()['token']
+s.get('https://cdn.79tian.com/api/wxapi/view/index.php?TokenVerify=%s'%token)
+signin=s.post('https://cdn.79tian.com/api/wxapi/view/ajax/sign.ajax.php?act=Signin').json()['msg']
+print(signin)
