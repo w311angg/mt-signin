e29da2014630fcb2f3b8ba727b23b5eae24de920
RZ <30381083+w311ang@users.noreply.github.com>
2021-04-08 07:24:14 +0800

    Update xc_signin.py

--- xc_signin.py
+++ xc_signin.py
@@ -1,10 +1,10 @@
 import requests
 import os
 
-s=requests.Session()
 username=os.getenv('xc_username')
 password=os.getenv('xc_password')
 
+s=requests.Session()
 s.get('https://cdn.79tian.com/api/wxapi/view/AppSwitch.php?inc=ajax_login&act=BannerHome')
 token=s.get('https://cdn.79tian.com/api/wxapi/view/AppSwitch.php?inc=ajax_login&act=login&username=%s&password=%s'%(username,password)).json()['token']
 s.get('https://cdn.79tian.com/api/wxapi/view/index.php?TokenVerify=%s'%token)
