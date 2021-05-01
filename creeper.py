ae849e2717ea07caf0f10abfe8259b3d96d8f132
RZ <30381083+w311ang@users.noreply.github.com>
2021-05-01 12:16:58 +0800

    Update creeper.py

--- creeper.py
+++ creeper.py
@@ -2,8 +2,8 @@ import requests
 import re
 import os
 
-username=os.getenv('username')
-password=os.getenv('password')
+username=os.getenv('cp_username')
+password=os.getenv('cp_password')
 
 s = requests.Session()
 s.headers.update({'user-agent': 'Mozilla/5.0 (Linux; Android 10; ONEPLUS A3010 Build/QQ3A.200805.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.152 Mobile Safari/537.36'})
