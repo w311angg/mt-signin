d535465a8209c49b8165ec795d1cd3c9c8c4fbfe
RZ <30381083+w311ang@users.noreply.github.com>
2021-06-15 20:18:16 +0800

    Update zhuan.py

--- zhuan.py
+++ zhuan.py
@@ -1,7 +1,8 @@
 import requests
 import os
 
-ppu=os.getenv('ppu')
+cookies=os.getenv('zhuan_cookie')
+cookies=base64.b64decode(cookies)
 
-re=requests.post('https://app.zhuanzhuan.com/zz/v2/zzpost/zhuanall',cookies={'PPU':ppu}).json()['respData']['toast']
+re=requests.post('https://app.zhuanzhuan.com/zz/v2/zzpost/zhuanall',headers={'cookie':cookies}).json()['respData']['toast']
 print(re)
