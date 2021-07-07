ec75512460a53bc08c0e6d3e7e480a101c1cd40d
w311ang <30381083+w311ang@users.noreply.github.com>
2021-07-07 12:28:57 +0800

    Update hkisl.py

--- hkisl.py
+++ hkisl.py
@@ -1,6 +1,10 @@
 import requests
+import pytools
 
 s=requests.Session()
-order=s.get('https://www.hkisl.net/cart.php?a=add&pid=218&language=chinese_cn').text
+url='https://www.hkisl.net/cart.php?a=add&pid=218&language=chinese_cn'
+content="""\
+<a href=\"%s\">立即前往</a>"""%url
+order=s.get(url).text
 if not '缺货中' in order:
-  
+  pytools.jmail('ISL_Check','免费VPS补货啦',content,html=True)
