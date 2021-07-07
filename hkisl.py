66721a73e9dba6754da09e54593200d1db7ed68a
w311ang <30381083+w311ang@users.noreply.github.com>
2021-07-07 12:49:46 +0800

    Update hkisl.py

--- hkisl.py
+++ hkisl.py
@@ -6,6 +6,6 @@ url='https://www.hkisl.net/cart.php?a=add&pid=218&language=chinese_cn'
 content="""\
 <a href=\"%s\">立即前往</a>"""%url
 order=s.get(url).text
-if True:
-#if not '缺货中' in order:
+#if True:
+if not '缺货中' in order:
   pytools.jmail('ISL_Check','免费VPS补货啦',content,html=True)
