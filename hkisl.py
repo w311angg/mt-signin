6870e0d3daa3578a2fbb81f1fa29192396fbcef9
w311ang <30381083+w311ang@users.noreply.github.com>
2021-07-08 00:00:12 +0800

    Update hkisl.py

--- hkisl.py
+++ hkisl.py
@@ -1,6 +1,12 @@
 import requests
 import pytools
 
+with open('hkisl.txt') as f:
+  try:
+    outofstock=f.read()
+  except FileNotFound:
+    outofstock=False
+
 s=requests.Session()
 url='https://www.hkisl.net/cart.php?a=add&pid=218&language=chinese_cn'
 content="""\
@@ -8,4 +14,10 @@ content="""\
 order=s.get(url).text
 #if True:
 if not '缺货中' in order:
+  outofstock=False
   pytools.jmail('ISL_Check','免费VPS补货啦',content,html=True)
+else:
+  outofstock=True
+
+with open('hkisl.txt','w') as f:
+  f.write(outofstock)
