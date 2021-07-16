f96568925785835cc715ebc7eccc0927e653f298
w311ang <30381083+w311ang@users.noreply.github.com>
2021-07-16 11:01:03 +0800

    Update zhuan.py

--- zhuan.py
+++ zhuan.py
@@ -5,5 +5,11 @@ import base64
 cookies=os.getenv('cookie')
 cookies=base64.b64decode(cookies)
 
-re=requests.post('https://app.zhuanzhuan.com/zz/v2/zzpost/zhuanall',headers={'cookie':cookies}).json()['respData']['toast']
-print(re)
+with requests.post('https://app.zhuanzhuan.com/zz/v2/zzpost/zhuanall',headers={'cookie':cookies}) as resp:
+  json=resp.json()
+  try:
+    re=json['respData']['toast']
+    print(re)
+  except TypeError:
+    raise Exception(json['errMsg']
+
