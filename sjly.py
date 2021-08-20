5ffa6e40f41e68735473c86b67a4d4aef6b4e032
w311ang <30381083+w311ang@users.noreply.github.com>
2021-08-20 13:40:02 +0800

    Update sjly.py

--- sjly.py
+++ sjly.py
@@ -1,9 +1,14 @@
 import requests
 import os
+import sys
 import xmltodict
 
 id=os.getenv('id')
 
-re=requests.get('http://tt.shouji.com.cn/appv3/xml_signed.jsp?versioncode=215&jsessionid='+id).text
+ren=requests.get('http://tt.shouji.com.cn/appv3/xml_signed.jsp?versioncode=215&jsessionid='+id)
+if ren.status_code==502:
+  print('服务器炸了')
+  sys.exit()
+re=ren.text
 ree = xmltodict.parse(re)
 print(ree['sjly']['info'])
