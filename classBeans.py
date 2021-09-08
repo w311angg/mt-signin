4a7fd6f148d5c35be4da53f2753a6feb4087bd40
w311ang <30381083+w311ang@users.noreply.github.com>
2021-09-08 22:01:51 +0800

    Create classBeans.py

---
+++ classBeans.py
@@ -0,0 +1,16 @@
+import requests, os
+
+token=os.getenv('token')
+
+s=requests.Session()
+s.headers.update({'version':'3.0.30','accesstoken':token})
+
+for i in range(15):
+  i+=1
+  with s.post('https://care.seewo.com/easicare/performance/score/student/69b0c3e1ab9b45768cecee4b1c5e1659',data='uid=rlqjzuovylxglhtjtnmqign74u119731&performanceId=f3d1501daa1c43a4b5e7515ea088c4b1&isNew=false&taskVersion=2&taskId=c4a7a660-45f7-4b6c-96ac-5f3ebf50d5e5',headers={'method':'POST','requesturi':'/easicare/performance/score/student/69b0c3e1ab9b45768cecee4b1c5e1659','content-type':'application/x-www-form-urlencoded; charset=utf-8'}) as resp:
+    json=resp.json()
+    code=json['statusCode']
+    if code==200:
+      print('成功点评第%s次'%i)
+
+import flower
