5937964f245aa96c6f9a02e62e4b5d27938df45a
w311ang <30381083+w311ang@users.noreply.github.com>
2021-09-10 23:21:05 +0800

    Update classBeans.py

--- classBeans.py
+++ classBeans.py
@@ -1,6 +1,7 @@
 import requests, os
 
 token=os.getenv('token')
+userid=os.getenv('userid')
 
 s=requests.Session()
 s.headers.update({'version':'3.0.30','accesstoken':token})
@@ -26,3 +27,22 @@ with s.post('https://care.seewo.com/easicare/account/v2/daily/rlqjzuovylxglhtjtn
     raise Exception('未知原因签到失败')
 
 import flower
+
+with s.post('https://care.seewo.com/easicare-mobile/apis.json?action=SHOP_TEACHER_INDEX_INIT',json={'actionName':'SHOP_TEACHER_INDEX_INIT','params':{'token':token}}) as resp:
+  json=resp.json()
+  data=json['data']['data']
+  unreceivedBeans=data['unreceivedBeans']
+  usableBeans=data['usableBeans']
+
+if unreceivedBeans!=0:
+  with s.post('https://care.seewo.com/easicare-mobile/apis.json?action=SHOP_INDEX_HANDLE',json={'actionName':'SHOP_INDEX_HANDLE','params':{'token':token,'exchangeBeansNum':unreceivedBeans,'userId':userid}}) as resp:
+    json=resp.json()
+    code=json['code']
+    if code==200:
+      print('成功领取%s个班优豆，现有%s个'%(unreceivedBeans,usableBeans))
+    elif code==40000:
+      print('经验值不足，无法领取')
+    else:
+      print('未知错误领取失败')
+else:
+  print('暂无班优豆可以领取')
