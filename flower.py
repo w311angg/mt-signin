1b9319be6836ab11ef67fe3e939ab78145c10fbc
w311ang <30381083+w311ang@users.noreply.github.com>
2022-08-02 14:53:34 +0800

    Update flower.py

--- flower.py
+++ flower.py
@@ -11,7 +11,7 @@ for i,num in zip(data,range(len(data))):
     #print(i)
     with s.post('https://care.seewo.com/easicare-mobile/apis.json?action=SHOP_CLASS_BUILD_GIFT_SEND',data=i,headers={'content-type':'application/json'}) as resp:
       json=resp.json()
-      code=json['data']['statusCode']
+      code=json['data']['statusCode'] if json['data']!=[] else json['code']
       if code==200:
         beans=json['data']['data']['teacherBeans']
         print('%s账号赠送成功'%num)
@@ -19,4 +19,5 @@ for i,num in zip(data,range(len(data))):
         print('已达到今日送花上限')
         break
       else:
-        raise Exception('未知原因送花失败')
+        msg=json['message']
+        raise Exception('送花失败: %s'%msg)
