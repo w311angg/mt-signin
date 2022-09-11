e3f745719012bf67af15a863504650fdc2826ed7
w311ang <30381083+w311ang@users.noreply.github.com>
2022-09-12 02:33:00 +0800

    Update repochecker.py

--- repochecker.py
+++ repochecker.py
@@ -8,31 +8,22 @@ usernames=['w311ang','w311angg']
 
 s=httpx.Client()
 headers={'authorization':'token '+token}
-nowrepos=set()
-oldrepos=set(jsonread('repos.json',set()))
+disabledrepos=jsonread('disabledrepos.json',[])
 
 for username in usernames:
   resp=s.get('https://api.github.com/users/%s/repos'%username,headers=headers)
   js=resp.json()
   for repo in js:
     name=repo['name']
-    nowrepos.add('%s/%s'%(username,name))
-jsondump(list(nowrepos),'repos.json')
-
-oldrepos=oldrepos|{'w311ang/AutoApiSecret-1'}
-deletedrepos=oldrepos-nowrepos
-disabledrepos=[]
-for repo in deletedrepos:
-  splited=repo.split('/')
-  username=splited[0]
-  reponame=splited[1]
-  resp=s.get('https://api.github.com/repos/%s/%s'%(username,reponame),headers=headers)
-  code=resp.status_code
-  if code==403:
-    js=resp.json()
-    msg=js['message']
-    reason=js['block']['reason']
-    disabledrepos.append(repo)
+    if not name in disabledrepos:
+      resp=s.get('https://api.github.com/repos/%s/%s'%(username,name),headers=headers)
+      code=resp.status_code
+      if code==403:
+        #js=resp.json()
+        #msg=js['message']
+        #reason=js['block']['reason']
+        disabledrepos.append(repo)
+jsondump(disabledrepos,'disabledrepos.json')
 
 if disabledrepos:
   jmail('Repo Checker','%s 仓库被封了!'%', '.join(disabledrepos),\
@@ -41,6 +32,4 @@ if disabledrepos:
 """%'\n'.join(['* https://github.com/%s'%repo for repo in disabledrepos])\
 )
 print('disabledrepos:',disabledrepos)
-print('deletedrepos:',deletedrepos)
-print('nowrepos:',nowrepos)
-print('oldrepos:',oldrepos)
+print('repos:',repos)
