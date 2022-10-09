1d2f2a7232c085e4a88ad565f4417b94e5137f37
w311ang <30381083+w311ang@users.noreply.github.com>
2022-10-09 20:19:50 +0800

    Update coolapkfollow.py

--- coolapkfollow.py
+++ coolapkfollow.py
@@ -55,7 +55,7 @@ for userid in followUser:
     timestamp=feed['dateline']
     if '#薅羊毛小分队' in message and (not feedid in feedsData[userid]):
       feedsData[userid].append(feedid)
-      newfeeds[userid].append({'username': username,'content': message.replace('<a class="feed-link-tag" href="/t/薅羊毛小分队?type=0">#薅羊毛小分队#</a> ','#薅羊毛小分队 ').replace('<a class="feed-link-tag" href="/t/薅羊毛小分队?type=0">#薅羊毛小分队#</a>','#薅羊毛小分队'), 'time': timeago.format(datetime.fromtimestamp(timestamp),datetime.now(),'zh_CN'), 'link': 'https://www.coolapk.com/feed/'+str(feedid)})
+      newfeeds[userid].append({'username': username,'content': message.replace('<a class="feed-link-tag" href="/t/薅羊毛小分队?type=0">#薅羊毛小分队#</a> ','').replace('<a class="feed-link-tag" href="/t/薅羊毛小分队?type=0">#薅羊毛小分队#</a>',''), 'time': timeago.format(datetime.fromtimestamp(timestamp),datetime.now(),'zh_CN'), 'link': 'https://www.coolapk.com/feed/'+str(feedid)})
 
 md=''
 for userid, data in newfeeds.items():
