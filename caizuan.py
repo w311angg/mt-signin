7e2ee90e1e9a15e031d51fe53b5e1e16184c533e
w311ang <30381083+w311ang@users.noreply.github.com>
2022-10-26 22:58:39 +0800

    Update caizuan.py

--- caizuan.py
+++ caizuan.py
@@ -12,12 +12,19 @@ s=httpx.Client()
 s.headers.update({'user-agent':'Mozilla/5.0 (Linux; Android 12; GM1910 Build/SKQ1.211113.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/4317 MMWEBSDK/20220903 Mobile Safari/537.36 MMWEBID/6025 MicroMessenger/8.0.28.2240(0x28001C35) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64','referer':'https://bai-piao.com/','x-requested-with':'com.tencent.mm'})
 
 def signin():
+  name='签到'
+
   resp=s.get('https://bai-piao.com/app/index.php?i=2&t=0&v=1.0&from=wxapp&c=entry&a=wxapp&do=user&m=skai_tooln_a&dopost=make_sign&userid=%s&openid=%s'%(userid,openid))
+  print('%s: %s'%(name,secretlog(resp.text))
   js=resp.json()
   if js['result']=='success':
-    print('签到成功!')
+    print('%s成功!'%name)
   else:
-    raise Exception('签到失败!\n%s'%secretlog(resp.text))
+    msg=js['msg']
+    if msg=='今天已经签到，请明天再来喔~':
+      print('%s: %s'%(name,msg))
+    else:
+      raise Exception('%s失败: %s'%(name,msg))
   return js
 
 def getUserInfo():
@@ -27,12 +34,16 @@ def getUserInfo():
   return js
 
 def makeStone(power):
+  name='开始采钻'
+
   resp=s.get('https://bai-piao.com/app/index.php?i=2&t=0&v=1.0&from=wxapp&c=entry&a=wxapp&do=index&m=skai_tooln_a&dopost=make_stone&userid=%s&openid=%s&make_power=%s'%(userid,openid,power))
+  print('%s: %s'%(name,secretlog(resp.text))
   js=resp.json()
   if js['result']=='success':
-    print('开始采钻成功!')
+    print('%s成功!'%name)
   else:
-    raise Exception('开始采钻失败!\n%s'%secretlog(resp.text))
+    msg=js['msg']
+    raise Exception('%s失败: %s'%(name,msg))
   return js
 
 userinfo=getUserInfo()
@@ -40,8 +51,10 @@ power=userinfo['userdata']['power']
 isMining=userinfo['make_stone_time']>0
 time.sleep(random.randint(2,9))
 signin()
-if not isMining:
+if isMining:
+  print('正在采钻中，跳过')
+elif power<=0:
+  print('没有能量，无法采钻')
+else:
   time.sleep(random.randint(2,9))
   makeStone(power)
-else:
-  print('正在采钻中，跳过')
