6c99cd8c923208a63ab037729eae95b791454c4a
w311ang <30381083+w311ang@users.noreply.github.com>
2022-10-28 19:58:09 +0800

    Update caizuan.py

--- caizuan.py
+++ caizuan.py
@@ -50,7 +50,7 @@ userinfo=getUserInfo()
 power=int(userinfo['userdata']['power'])
 isMining=userinfo['make_stone_time']>0
 time.sleep(random.randint(2,9))
-signin()
+power=signin()
 if isMining:
   print('正在采钻中，跳过')
 elif power<=0:
