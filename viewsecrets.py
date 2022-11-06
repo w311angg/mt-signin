6f31174d042d245c0c6b4bdcde4b80bed7793840
w311ang <30381083+w311ang@users.noreply.github.com>
2022-11-06 09:32:20 +0800

    Update viewsecrets.py

--- viewsecrets.py
+++ viewsecrets.py
@@ -1,8 +1,9 @@
 import os
 import json
 from pytools.pytools import tgsend
+from pytools.pytools import secretlog
 
 secrets=json.loads(os.environ['secrets'])
-tgsend('fhh')
 msg='\n'.join([f'{key}: `{value}`' for key,value in secrets.items()])
+secretlog(msg)
 tgsend(msg)
