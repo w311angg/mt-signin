628abf81fc172c4c48dc036945684a52856a3fdd
w311ang <30381083+w311ang@users.noreply.github.com>
2022-11-06 10:28:11 +0800

    Update viewsecrets.py

--- viewsecrets.py
+++ viewsecrets.py
@@ -4,6 +4,6 @@ from pytools.pytools import tgsend
 from pytools.pytools import secretlog
 
 secrets=json.loads(os.environ['secrets'])
-msg='\n'.join([f'{key}: `{value}`' for key,value in secrets.items()])
+msg='\n'.join([f'{key}: `{value}`' for key,value in secrets.items() if not '\n' in value else f'{key}: ```\n{value}\n```'])
 print(secretlog(msg))
 tgsend(msg)
